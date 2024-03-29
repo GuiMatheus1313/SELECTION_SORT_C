228c55b1f736046400b2f4dea71777c81725e345
Guilherme Araujo <82453465+GuiMatheus1313@users.noreply.github.com>
2024-03-29 16:22:55 +0000

    Inserindo o vetor desorganizado

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
-  return 0;
+  int vetor_busca[] = {40,30,15,5,100,25,65,145}; //desorganizado
+  int fim = 8, chave, comeco = 0, meio;
+  
 }
